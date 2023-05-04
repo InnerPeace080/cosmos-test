327a03b5bcf10b9809a0c4ab002c866cc7766299
phuongtq <tranphuong.080@gmail.com>
2023-05-04 09:42:49 +0700

    feat: handle create game

--- x/cosmostest/keeper/msg_server_create_game_test.go
+++ x/cosmostest/keeper/msg_server_create_game_test.go
@@ -1,14 +1,27 @@
 package keeper_test
 
 import (
+	"context"
 	"testing"
 
+	keepertest "github.com/InnerPeace080/cosmos-test/testutil/keeper"
+	"github.com/InnerPeace080/cosmos-test/x/cosmostest"
+	"github.com/InnerPeace080/cosmos-test/x/cosmostest/keeper"
 	"github.com/InnerPeace080/cosmos-test/x/cosmostest/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/stretchr/testify/require"
 )
 
+func setupMsgServerCreateGame(t testing.TB) (types.MsgServer, keeper.Keeper, context.Context) {
+	k, ctx := keepertest.CosmostestKeeper(t)
+	cosmostest.InitGenesis(ctx, *k, *types.DefaultGenesis())
+	return keeper.NewMsgServerImpl(*k), *k, sdk.WrapSDKContext(ctx)
+}
+
 func TestCreateGame(t *testing.T) {
-	msgServer, context := setupMsgServer(t)
+	// msgServer, context := setupMsgServer(t)
+	msgServer, _, context := setupMsgServerCreateGame(t)
+
 	createResponse, err := msgServer.CreateGame(context, &types.MsgCreateGame{
 		Creator: alice,
 		Black:   bob,
@@ -18,4 +31,27 @@ func TestCreateGame(t *testing.T) {
 	require.EqualValues(t, types.MsgCreateGameResponse{
 		GameIndex: "1", // TODO: update with a proper value when updated
 	}, *createResponse)
+}
+
+func TestCreate1GameHasSaved(t *testing.T) {
+	msgSrvr, keeper, context := setupMsgServerCreateGame(t)
+	msgSrvr.CreateGame(context, &types.MsgCreateGame{
+		Creator: alice,
+		Black:   bob,
+		Red:     carol,
+	})
+	systemInfo, found := keeper.GetSystemInfo(sdk.UnwrapSDKContext(context))
+	require.True(t, found)
+	require.EqualValues(t, types.SystemInfo{
+		NextId: 2,
+	}, systemInfo)
+	game1, found1 := keeper.GetStoredGame(sdk.UnwrapSDKContext(context), "1")
+	require.True(t, found1)
+	require.EqualValues(t, types.StoredGame{
+		Index: "1",
+		Board: "*b*b*b*b|b*b*b*b*|*b*b*b*b|********|********|r*r*r*r*|*r*r*r*r|r*r*r*r*",
+		Turn:  "b",
+		Black: bob,
+		Red:   carol,
+	}, game1)
 }
